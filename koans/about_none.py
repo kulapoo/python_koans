8ee421218ddc13fafdb522d401411697095aa2b5
kulapoo <kulaskulapoo@gmail.com>
2024-11-08 17:55:32 +0800

    reset to original to lookback repeat the exercises

--- koans/about_none.py
+++ koans/about_none.py
@@ -11,11 +11,11 @@ class AboutNone(Koan):
 
     def test_none_is_an_object(self):
         "Unlike NULL in a lot of languages"
-        self.assertEqual(True, isinstance(None, object))
+        self.assertEqual(__, isinstance(None, object))
 
     def test_none_is_universal(self):
         "There is only one None"
-        self.assertEqual(True, None is None)
+        self.assertEqual(____, None is None)
 
     def test_what_exception_do_you_get_when_calling_nonexistent_methods(self):
         """
@@ -27,7 +27,7 @@ class AboutNone(Koan):
         Don't worry about what 'try' and 'except' do, we'll talk about this later
         """
         try:
-            None.some_method_none_does_not_know_about() # type: ignore
+            None.some_method_none_does_not_know_about()
         except Exception as ex:
             ex2 = ex
 
@@ -37,15 +37,15 @@ class AboutNone(Koan):
         #
         #     https://github.com/gregmalcolm/python_koans/wiki/Class-Attribute
 
-        self.assertEqual(AttributeError, ex2.__class__) # type: ignore
+        self.assertEqual(__, ex2.__class__)
 
         # What message was attached to the exception?
         # (HINT: replace __ with part of the error message.)
-        self.assertRegex(ex2.args[0], "'NoneType' object has no attribute 'some_method_none_does_not_know_about'") # type: ignore
+        self.assertRegex(ex2.args[0], __)
 
     def test_none_is_distinct(self):
         """
         None is distinct from other things which are False.
         """
-        self.assertEqual(True, None is not 0)
-        self.assertEqual(True, None is not False)
+        self.assertEqual(__, None is not 0)
+        self.assertEqual(__, None is not False)
