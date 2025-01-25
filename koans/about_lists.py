5c4a770134b8cf1a8f4b1ffdeb0feae98cafbb45
kulapoo <kulaskulapoo@gmail.com>
2025-01-26 03:28:47 +0800

    python daily exercise

--- koans/about_lists.py
+++ koans/about_lists.py
@@ -29,18 +29,18 @@ class AboutLists(Koan):
     def test_accessing_list_elements(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual("peanut", noms[0])
-        self.assertEqual("jelly", noms[3])
-        self.assertEqual("jelly", noms[-1])
-        self.assertEqual("butter", noms[-3])
+        self.assertEqual('peanut', noms[0])
+        self.assertEqual('jelly', noms[3])
+        self.assertEqual('jelly', noms[-1])
+        self.assertEqual('butter', noms[-3])
 
     def test_slicing_lists(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(["peanut"], noms[0:1])
-        self.assertEqual(["peanut", "butter"], noms[0:2])
+        self.assertEqual(['peanut'], noms[0:1])
+        self.assertEqual(['peanut', 'butter'], noms[0:2])
         self.assertEqual([], noms[2:2])
-        self.assertEqual(["and", "jelly"], noms[2:20])
+        self.assertEqual(['and', 'jelly'], noms[2:20])
         self.assertEqual([], noms[4:0])
         self.assertEqual([], noms[4:100])
         self.assertEqual([], noms[5:0])
@@ -58,7 +58,7 @@ class AboutLists(Koan):
         self.assertEqual([5, 6, 7, 8], list(range(5, 9)))
 
     def test_ranges_with_steps(self):
-        self.assertEqual([5,4], list(range(5, 3, -1)))
+        self.assertEqual([5, 4], list(range(5, 3, -1)))
         self.assertEqual([0, 2, 4, 6], list(range(0, 8, 2)))
         self.assertEqual([1, 4, 7], list(range(1, 8, 3)))
         self.assertEqual([5, 1, -3], list(range(5, -7, -4)))
@@ -67,7 +67,7 @@ class AboutLists(Koan):
     def test_insertions(self):
         knight = ['you', 'shall', 'pass']
         knight.insert(2, 'not')
-        self.assertEqual(['you', 'shall',  'not', 'pass'], knight)
+        self.assertEqual(['you', 'shall', 'not', 'pass'], knight)
 
         knight.insert(0, 'Arthur')
         self.assertEqual(['Arthur', 'you', 'shall', 'not', 'pass'], knight)
@@ -102,8 +102,7 @@ class AboutLists(Koan):
 
         popped_value = queue.pop(0)
         self.assertEqual(1, popped_value)
-        self.assertEqual([2,'last'], queue)
+        self.assertEqual([2, 'last'], queue)
 
         # Note, popping from the left hand side of a list is
         # inefficient. Use collections.deque instead.
-
--- koans/triangle.py
+++ koans/triangle.py
@@ -18,7 +18,29 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError()
+
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise TriangleError()
+
+
+
+    set1 = set([a, b, c])
+    set2 = set([a, c, b])
+
+    set3 = set1 & set2
+
+    set_len = len(set3)
+
+    if set_len == 1:
+        return "equilateral"
+    elif set_len == 2:
+        return "isosceles"
+    else:
+        return "scalene"
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
